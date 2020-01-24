38028b725828acdf3daff3eec84e3e2365c11db0
jack culpan <jackculpan@me.com>
2020-01-24 21:15:37 +0000

    added image support

--- app.py
+++ app.py
@@ -35,6 +35,30 @@ def send_message(sender_id, message_text):
         "message": {"text": message_text}
     }))
 
+
+def send_image(sender_id, image_url):
+    '''
+    Sending response back to the user using facebook graph API
+    '''
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+
+        params={"access_token": PAGE_ACCESS_TOKEN},
+
+        headers={"Content-Type": "application/json"},
+
+        data=json.dumps({
+        "recipient": {"id": sender_id},
+        "message": {
+          "attachment":{
+            "type":"image",
+            "payload":{
+              "url":image_url,
+              "is_reusable":true
+            }
+          }
+        }
+    }))
+
 @app.route('/', methods=['POST'])
 def handle_message():
     '''
@@ -46,14 +70,14 @@ def handle_message():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
                 if messaging_event.get("message"):
-
                     sender_id = messaging_event["sender"]["id"]
                     recipient_id = messaging_event["recipient"]["id"]
                     message_text = messaging_event["message"]["text"]
                     send_message_response(sender_id, message_text)
-                    if messaging_event["message"].get("attachments"):
-                        attachment_link = messaging_event["message"]["attachments"][0]["payload"]["url"]
-                        send_message_response(sender_id, attachment_link)
+
+                    if messaging_event["message"].get("attachment"):
+                        attachment_link = messaging_event["message"]["attachment"]["payload"]["url"]
+                        send_image(sender_id, attachment_link)
     return "ok"
 
 def send_message_response(sender_id, message_text):
@@ -67,20 +91,26 @@ def send_message_response(sender_id, message_text):
 @app.route('/webhook_dev', methods=['POST'])
 def webhook_dev():
     # custom route for local development
-    data = json.loads(request.data.decode('utf-8'))
-    user_message = data['entry'][0]['messaging'][0]['message']['text']
-    user_id = data['entry'][0]['messaging'][0]['sender']['id']
-    response = {
-        'recipient': {'id': user_id},
-        'message': {'text': handle_message(user_id, user_message)}
-    }
-    return Response(
-        response=json.dumps(response),
-        status=200,
-        mimetype='application/json'
-    )
+    #data = json.loads(request.data.decode('utf-8'))
+    #user_message = data['entry'][0]['messaging'][0]['message']['text']
+    #user_id = data['entry'][0]['messaging'][0]['sender']['id']
+    data = request.get_json()
 
-def handle_message(user_id, user_message):
+    #if data["object"] == "page":
+    for entry in data["entry"]:
+        for messaging_event in entry["messaging"]:
+            if messaging_event.get("message"):
+                sender_id = messaging_event["sender"]["id"]
+                recipient_id = messaging_event["recipient"]["id"]
+                message_text = messaging_event["message"]["text"]
+                send_message_response(sender_id, message_text)
+
+                if messaging_event["message"].get("attachment"):
+                    attachment_link = messaging_event["message"]["attachment"]["payload"]["url"]
+                    send_image(sender_id, attachment_link)
+    return "ok"
+
+def handle_dev_message(user_id, user_message):
     # DO SOMETHING with the user_message ... ¯\_(ツ)_/¯
     return "Hello "+user_id+" ! You just sent me : " + user_message
 
